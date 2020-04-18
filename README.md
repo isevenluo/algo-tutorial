b545d333d095c8e948bfb9e70f3dabe9285a15cc
cc <pekxxoo@163.com>
2020-04-19 00:07:09 +0800

    补充leetcode对应的题目序号，强化训练

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# 面试必知必会的数据结构和算法基本实现，示例代码目前均为Java实现
-
+# 面试必知必会的数据结构和算法基本实现
+> 示例代码目前均为Java实现
 
 #### 扫描下方二维码，关注我的微信公众号“罗小黑爱编程”。
 #### 回复“666”领取我为你精心准备的《BAT大厂面试通关手册》和 3T Java通关视频教程。
@@ -9,9 +9,16 @@
 ## 数组
 
 ## 链表
-
+> 练习题LeetCode对应编号：206，141，21，19，876
 - [单链表反转](./src/org/luoxiaohei/java/linkedlist/LinkedListAlgo.java)
 - [链表中环的检测](./src/org/luoxiaohei/java/linkedlist/LinkedListAlgo.java)
 - [两个有序的链表合并](./src/org/luoxiaohei/java/linkedlist/LinkedListAlgo.java)
 - [删除链表倒数第 n 个结点](./src/org/luoxiaohei/java/linkedlist/LinkedListAlgo.java)
 - [求链表的中间结点](./src/org/luoxiaohei/java/linkedlist/LinkedListAlgo.java)
+
+## 栈
+> leetcode上关于栈的题目大家可以先做20,155,232,844,224,682,496.
+
+- 用数组实现一个顺序栈
+- 用链表实现一个顺序栈
+- 实现一个浏览器的前进后退功能
---
+++ src/org/luoxiaohei/java/stack/StackBaseOnArray.java
@@ -0,0 +1,67 @@
+package org.luoxiaohei.java.stack;
+
+import java.util.Arrays;
+
+/**
+ * @description: 利用数组实现一个顺序栈，也可以用链表实现链式栈
+ * 栈结构只允许操作栈顶元素，所以使用栈中元素count的加减来实现push和pop操作
+ * 时间空间复杂度均为O（1）
+ * @outhor: coderluo
+ * @create: 2020-03-27 0:41
+ */
+public class StackBaseOnArray {
+    // 数组
+    private String[] items;
+
+    // 数组中元素的个数
+    private int count;
+    // 数组的大小
+    private int n;
+
+    public StackBaseOnArray(int n) {
+        this.items = new String[n];
+        this.count = 0;
+        this.n = n;
+    }
+
+    public boolean push(String item) {
+        // 数组满了，入栈失败
+        if (count == n) {
+            return false;
+        }
+        // 将入栈元素放到count位置
+        items[count++] = item;
+        return true;
+    }
+
+    public String pop() {
+        // 数组为空，返回null
+        if (count == 0) {
+            return null;
+        }
+        // 返回count-1位置的元素
+        return items[--count];
+    }
+
+    public static void main(String[] args) {
+        StackBaseOnArray stack = new StackBaseOnArray(3);
+        stack.push("a");
+        stack.push("b");
+        stack.push("c");
+        stack.push("c");
+        System.out.println(stack);
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+
+
+    }
+
+    @Override
+    public String toString() {
+        return "StackImpl{" +
+                "items=" + Arrays.toString(items) +
+                '}';
+    }
+}
