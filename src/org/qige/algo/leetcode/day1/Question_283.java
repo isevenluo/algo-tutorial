b93a41327a75e4e46806d66cd87d8794ee28539d
sevenluo <isevenluo@163.com>
2021-11-01 23:56:45 +0800

    leetcode 双指针技巧，原地修改数组类题目解答

---
+++ src/org/qige/algo/leetcode/day1/Question_283.java
@@ -0,0 +1,41 @@
+package org.qige.algo.leetcode.day1;
+
+import org.junit.Test;
+
+/**
+ * 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+ * 输入: [0,1,0,3,12]
+ * 输出: [1,3,12,0,0]
+ * 说明:
+ * 必须在原数组上操作，不能拷贝额外的数组。
+ * 尽量减少操作次数。
+ */
+public class Question_283 {
+
+    public void moveZeroes(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return;
+        }
+        int fast = 0, slow = 0;
+        while (fast < nums.length) {
+            if (nums[fast] != 0) {
+                nums[slow] = nums[fast];
+                slow++;
+            }
+            fast++;
+        }
+        while (slow < fast) {
+            nums[slow] = 0;
+            slow++;
+        }
+    }
+
+    @Test
+    public void test() {
+        int[] nums = new int[]{0, 1, 0, 3, 12};
+        moveZeroes(nums);
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i]);
+        }
+    }
+}
---
+++ src/org/qige/algo/leetcode/day1/Question_83.java
@@ -0,0 +1,57 @@
+package org.qige.algo.leetcode.day1;
+
+/**
+ * @微信公众号: 七哥聊编程
+ * @github: https://github.com/isevenluo
+ * @gitee: https://gitee.com/isevenluo
+ * @名额不多的个人微信：qige777ya
+ * @description: 存在一个按升序排列的链表，给你这个链表的头节点 head ，请你删除所有重复的元素，使每个元素 只出现一次。
+ * 返回同样按升序排列的结果链表。
+ * <p>
+ * 链表中节点数目在范围 [0, 300] 内
+ * -100 <= Node.val <= 100
+ * 题目数据保证链表已经按升序排列
+ * @author: 程序员七哥
+ * @create: 2021-11-01 23:03
+ **/
+public class Question_83 {
+
+    /**
+     * 输入：head = [1,1,2,3,3]
+     * 输出：[1,2,3]
+     */
+    public ListNode deleteDuplicates(ListNode head) {
+       if (head == null || head.next == null) {
+           return head;
+       }
+       ListNode slow = head, fast = head;
+       while (fast.next != null) {
+           if (fast.val != fast.next.val) {
+               slow.next = fast.next;
+               slow = slow.next;
+           }
+           fast = fast.next;
+       }
+       slow.next = null;
+       return head;
+    }
+
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+}
