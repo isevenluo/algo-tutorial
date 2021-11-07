3fd7abb0bbfbdd8c1d7011ab87ae21077c8b7b22
sevenluo <isevenluo@163.com>
2021-11-07 23:21:24 +0800

    滑动窗口算法，感觉没学会，明天继续吧，今天困了

---
+++ src/org/qige/algo/leetcode/day7/Leetcode_3.java
@@ -0,0 +1,46 @@
+package org.qige.algo.leetcode.day7;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @微信公众号: 七哥聊编程
+ * @github: https://github.com/isevenluo
+ * @gitee: https://gitee.com/isevenluo
+ * @名额不多的个人微信：qige777ya
+ * @description:
+ * @author: 程序员七哥
+ * @create: 2021-11-07 23:11
+ **/
+public class Leetcode_3 {
+
+    public int lengthOfLongestSubstring(String s) {
+        if (s.length()==0) {
+            return 0;
+        }
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+        int max = 0;
+        int left = 0;
+        for(int i = 0; i < s.length(); i ++){
+            if(map.containsKey(s.charAt(i))){
+                left = Math.max(left,map.get(s.charAt(i)) + 1);
+            }
+            map.put(s.charAt(i),i);
+            max = Math.max(max,i-left+1);
+        }
+        return max;
+
+    }
+
+    /**
+     * 输入: s = "abcabcbb"
+     * 输出: 3
+     * 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+     */
+    @Test
+    public void test() {
+        System.out.println(lengthOfLongestSubstring("abcabcbb"));
+    }
+}
